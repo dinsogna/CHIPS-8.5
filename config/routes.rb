32b9b66d4ce79da22be8b247d975b62f13c04eb7
dinsogna <62686152+dinsogna@users.noreply.github.com>
2021-10-26 22:52:51 -0400

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rottenpotatoes::Application.routes.draw do
+  resources :movies
+  # map '/' to be a redirect to '/movies'
+  get '/search' => 'movies#search_tmdb', as: 'search_tmdb'
+  post '/add_movie' => 'movies#add_movie', as: "add_movie"
+  root :to => redirect('/movies')
+end
